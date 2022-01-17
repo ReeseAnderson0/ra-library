9ce562e4f4a4dc39d26762c52f9ae900874393d0
Reese Anderson <reese.anderson@onmail.com>
2022-01-17 14:12:00 -0500

    fixed syntax

--- db/migrate/20220111203903_create_books_users.rb
+++ db/migrate/20220111203903_create_books_users.rb
@@ -1,6 +1,6 @@
 class CreateBooksUsers < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :books, :users, , column_options: { null: false, foreign_key: true } do |t|
+    create_join_table :books, :users, column_options: { null: false, foreign_key: true } do |t|
       t.index [:user_id, :book_id]
       t.index [:book_id, :user_id]
       
