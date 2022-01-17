a30935f15482fd5b4eec3984224f628c595126bc
Reese Anderson <reese.anderson@onmail.com>
2022-01-17 13:46:58 -0500

    primary id

--- db/migrate/20220111203903_create_books_users.rb
+++ db/migrate/20220111203903_create_books_users.rb
@@ -1,6 +1,7 @@
 class CreateBooksUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :books, :users do |t|
+      t.primary_key :id
       t.references :user, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
       
