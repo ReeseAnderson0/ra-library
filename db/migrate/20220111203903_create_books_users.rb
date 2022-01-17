fec8c1329b8c2f887885a75cee315b650c6476b1
Reese Anderson <reese.anderson@onmail.com>
2022-01-17 14:09:21 -0500

    changed ref to index

--- db/migrate/20220111203903_create_books_users.rb
+++ db/migrate/20220111203903_create_books_users.rb
@@ -1,9 +1,8 @@
 class CreateBooksUsers < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :books, :users do |t|
-      t.primary_key :id
-      t.references :user, null: false, foreign_key: true
-      t.references :book, null: false, foreign_key: true
+    create_join_table :books, :users, , column_options: { null: false, foreign_key: true } do |t|
+      t.index [:user_id, :book_id]
+      t.index [:book_id, :user_id]
       
       t.timestamps
     end
