0d0c60088ac61364fb248be746ac4ec17f05bba0
Reese Anderson <reese.anderson@onmail.com>
2022-01-17 13:53:32 -0500

    test join

--- db/migrate/20220111203903_create_books_users.rb
+++ db/migrate/20220111203903_create_books_users.rb
@@ -1,6 +1,6 @@
 class CreateBooksUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :books, :users do |t|
+    create_join_table :books, :users do |t|
       t.primary_key :id
       t.references :user, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
